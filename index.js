d1f92ff97e6aa7b697588c23029d33b47d847ed7
James Lawrence <jameslaw195@gmail.com>
2024-01-08 14:19:16 +0100

    working

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 import express from "express";
 
-const PORT = process.env.port || 3000;
+// import path from "path";
+
+const PORT = process.env.PORT || 3000;
 const app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/", function (req, res) {
+  console.log("serving home page");
+  res.render("homepage");
+});
+app.get("/echo/:message", (req, res) => {
+  const message = req.params.message;
+  res.json({
+    message: message,
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Hello Listening on port ${PORT}`);
